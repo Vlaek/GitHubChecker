94d73bc7ecf8bfdae3aff5ad5a1998d4b3520e76
Vladislav Eichwald <vlad.eichwald@gmail.com>
2024-08-11 11:03:58 +0700

    rename containers

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { Footer, Header, Main } from '../'
-import { Layout } from '../../container'
+import { Layout } from '../../containers'
 
 const App: FC = () => {
 	return <Layout header={<Header />} main={<Main />} footer={<Footer />} />
